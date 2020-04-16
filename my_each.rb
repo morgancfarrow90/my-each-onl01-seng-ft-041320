df078f905a6a75fdf8344fe746893df4480dd3ed
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-16 20:05:54 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    i = 0 
+  
+  while i < 4 
+    yield array [i]
+    i = i + 1 
+  end
+  array 
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+    puts i
+  end
 end
